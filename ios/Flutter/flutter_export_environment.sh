8be163c872739d94058ad7b5f6cfdaf37b461f5e
Nico <102190013+NicoCieri10@users.noreply.github.com>
2023-11-01 23:02:54 -0300

    fix errors on project

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Nico\Documents\GitHub\fidoo_challenge\fidooo_challenge"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Nico\Documents\GitHub\fidooo_challenge\fidooo_challenge"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
